d0bcef26b3c7b836ddef4a97e8db3667709e22e5
open-mind <316257774@qq.com>
2018-08-16 15:30:50 +0800

    完成主体逻辑，还需后续补充

--- hit/__main__.py
+++ hit/__main__.py
@@ -9,23 +9,36 @@ def main():
     print args
     if args[0] == 'push':
         remoteRepo = RemoteRepo()
-        remoteUrl =
         # 获取远程地址
+        remoteHash = remoteRepo.getRemoteHash()
+        remoteUrl = remoteRepo.getRemoteUrl()
         # 获取远程仓库：ipfs get 远程地址
+        ipfsGetRepoCmd = "ipfs get %s" % remoteHash # 要重命名
+        os.system(ipfsGetRepoCmd)
         # 将版本库提交到本地仓库
-        remoteTimeStamp = '' # 获取远程仓库的时间戳remoteTimeStamp
+        pathLocalRemoteRepo = "" # 远程仓库在本地的存放位置
+        gitPushCmd = "git push %s" % (pathLocalRemoteRepo)
+        for arg in args[1:]:
+            gitPushCmd += " " + arg # 这里注意，如果用户添加了地址，这里没有去除
+        os.system(gitPushCmd)
+        # 获取远程仓库的时间戳remoteTimeStamp
+        remoteTimeStamp = "ipfs get %s/timestamp" % remoteHash
         if RemoteRepo.timeStamp == remoteTimeStamp:
-        #     在仓库中添加本地的时间戳
-        #     将合并后的代码仓库提交到ipfs网络中: ipfs add -r
-        # 获取新提交的仓库rootFileUrl，并将其命名到名字空间中
+            # 在仓库中添加本地的时间戳
+            # 将合并后的代码仓库提交到ipfs网络中: ipfs add -r
+            os.system("ipfs add -r") # 要进入仓库路径中
+            # 获取新提交的仓库rootFileUrl，并将其命名到名字空间中
         else:
             print "Err: The remote repo has been updated by other user, please push the repo again"
+
+        # 删除本地的远程仓库
+        os.system("rm -rf %s" % pathLocalRemoteRepo)
 
     else:
         cmd = "git"
         for arg in args:
             cmd += " " + arg
-    	os.system(cmd)
+        os.system(cmd)
     
 if __name__ == '__main__':
     main()
--- hit/classmodule.py
+++ hit/classmodule.py
@@ -4,17 +4,22 @@ class RemoteRepo():
         gitRemoteCmd = "git remote get-url --all origin"
         gitRemote = os.popen(gitRemoteCmd).read()
         gitRemoteSplit = gitRemote.split("/")
-        if gitRemoteSplit[-2] == 'ipfs' or gitRemoteSplit[-2] == 'ipns':
+        if gitRemoteSplit[-2] == 'ipns':
+            self.remoteUrl = gitRemote
             self.remoteHash = gitRemoteSplit[-1]
             ipfsCatTimeStamp = "ipfs cat %s/timestamp" % (self.remoteUrl)
             self.timeStamp = os.popen(ipfsCatTimeStamp).read()
         else:
             self.remoteHash = ""
             self.timeStamp = ""
+            self.remoteUrl = ""
         self.rootFileUrl = ""
 
-    def getHashUrlByRemote(self):
+    def getRemoteHash(self):
         return self.remoteHash
+
+    def getRemoteUrl(self):
+        return self.remoteUrl
 
     def getTimeStamp(self):
         return self.timeStamp
