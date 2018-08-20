0cfcc17193d7c933bb73a8d1b621dfd2e4da172e
open-mind <316257774@qq.com>
2018-08-20 16:46:46 +0800

    去除读取信息的换行符

--- hit/classmodule.py
+++ hit/classmodule.py
@@ -3,14 +3,14 @@ class RemoteRepo():
         import os
         print "hit get remote url"
         gitRemoteCmd = "git remote get-url --all origin"
-        gitRemote = os.popen(gitRemoteCmd).read()
+        gitRemote = os.popen(gitRemoteCmd).read().strip('\n')
         gitRemoteSplit = gitRemote.split("/")
         if gitRemoteSplit[-2] == 'ipns':
             self.remoteUrl = gitRemote
             self.remoteHash = gitRemoteSplit[-1]
             print gitRemoteSplit
             print "hit resolve ipns name"
-            remoteFileUrlList = os.popen("ipfs resolve /ipns/%s" % self.remoteHash).read().split("/")
+            remoteFileUrlList = os.popen("ipfs resolve /ipns/%s" % self.remoteHash).read().strip("\n").split("/")
             if remoteFileUrlList[1] == "ipfs":
                 self.remoteFileHash = remoteFileUrlList[-1]
                 print "hit get ipfs file key"
