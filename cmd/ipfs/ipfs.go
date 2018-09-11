1dd02ffdaff0ee1a2cba781bf7f76a9db2911ba2
Nigel <609441862@qq.com>
2018-09-11 17:05:30 +0800

    add write file in cmd/ipfs/ipfs.go init function

---
+++ cmd/ipfs/ipfs.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ipfs/go-ipfs/core/commands"
+	"io"
+	"os"
+
+	"gx/ipfs/QmPTfgFTo9PFr1PvPKyKoeMgBvYPh6cX3aDP7DHKVbnCbi/go-ipfs-cmds"
+)
+
+// This is the CLI root, used for executing commands accessible to CLI clients.
+// Some subcommands (like 'ipfs daemon' or 'ipfs init') are only accessible here,
+// and can't be called through the HTTP API.
+var Root = &cmds.Command{
+	Options:  commands.Root.Options,
+	Helptext: commands.Root.Helptext,
+}
+
+// commandsClientCmd is the "ipfs commands" command for local cli
+var commandsClientCmd = commands.CommandsCmd(Root)
+
+// Commands in localCommands should always be run locally (even if daemon is running).
+// They can override subcommands in commands.Root by defining a subcommand with the same name.
+var localCommands = map[string]*cmds.Command{
+	"daemon":   daemonCmd,
+	"init":     initCmd,
+	"commands": commandsClientCmd,
+}
+
+// add by Nigel start: check file exists, check the error
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+// add by Nigel end
+
+
+func init() {
+	// setting here instead of in literal to prevent initialization loop
+	// (some commands make references to Root)
+	Root.Subcommands = localCommands
+
+	// add by Nigel start: write a file
+	fmt.Println("!!!!!!!!!!!!!In init function")
+	var f *os.File
+	var err1 error
+	// write a server ip to a local file
+	var clientFilePath = "./serverIp.txt"
+	if checkFileIsExist(clientFilePath) {
+		f, err1 = os.OpenFile(clientFilePath, os.O_APPEND, 0666) //打开文件
+		fmt.Println("文件存在")
+	} else {
+		f, err1 = os.Create(clientFilePath)
+		fmt.Println("文件不存在")
+	}
+	check(err1)
+	n, err1 := io.WriteString(f, "47.105.76.115")
+	check(err1)
+	fmt.Printf("写入 %d 个字节n", n)
+	// add by Nigel end
+
+	for k, v := range commands.Root.Subcommands {
+		if _, found := Root.Subcommands[k]; !found {
+			Root.Subcommands[k] = v
+		}
+	}
+}
+
+// NB: when necessary, properties are described using negatives in order to
+// provide desirable defaults
+type cmdDetails struct {
+	cannotRunOnClient bool
+	cannotRunOnDaemon bool
+	doesNotUseRepo    bool
+
+	// doesNotUseConfigAsInput describes commands that do not use the config as
+	// input. These commands either initialize the config or perform operations
+	// that don't require access to the config.
+	//
+	// pre-command hooks that require configs must not be run before these
+	// commands.
+	doesNotUseConfigAsInput bool
+
+	// preemptsAutoUpdate describes commands that must be executed without the
+	// auto-update pre-command hook
+	preemptsAutoUpdate bool
+}
+
+func (d *cmdDetails) String() string {
+	return fmt.Sprintf("on client? %t, on daemon? %t, uses repo? %t",
+		d.canRunOnClient(), d.canRunOnDaemon(), d.usesRepo())
+}
+
+func (d *cmdDetails) Loggable() map[string]interface{} {
+	return map[string]interface{}{
+		"canRunOnClient":     d.canRunOnClient(),
+		"canRunOnDaemon":     d.canRunOnDaemon(),
+		"preemptsAutoUpdate": d.preemptsAutoUpdate,
+		"usesConfigAsInput":  d.usesConfigAsInput(),
+		"usesRepo":           d.usesRepo(),
+	}
+}
+
+func (d *cmdDetails) usesConfigAsInput() bool { return !d.doesNotUseConfigAsInput }
+func (d *cmdDetails) canRunOnClient() bool    { return !d.cannotRunOnClient }
+func (d *cmdDetails) canRunOnDaemon() bool    { return !d.cannotRunOnDaemon }
+func (d *cmdDetails) usesRepo() bool          { return !d.doesNotUseRepo }
+
+// "What is this madness!?" you ask. Our commands have the unfortunate problem of
+// not being able to run on all the same contexts. This map describes these
+// properties so that other code can make decisions about whether to invoke a
+// command or return an error to the user.
+var cmdDetailsMap = map[string]cmdDetails{
+	"init":        {doesNotUseConfigAsInput: true, cannotRunOnDaemon: true, doesNotUseRepo: true},
+	"daemon":      {doesNotUseConfigAsInput: true, cannotRunOnDaemon: true},
+	"commands":    {doesNotUseRepo: true},
+	"version":     {doesNotUseConfigAsInput: true, doesNotUseRepo: true}, // must be permitted to run before init
+	"log":         {cannotRunOnClient: true},
+	"diag/cmds":   {cannotRunOnClient: true},
+	"repo/fsck":   {cannotRunOnDaemon: true},
+	"config/edit": {cannotRunOnDaemon: true, doesNotUseRepo: true},
+}
