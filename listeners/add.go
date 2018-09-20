8582da49ca87857f204aa724f525eb7bae2020e5
Nigel <609441862@qq.com>
2018-09-20 22:28:47 +0800

    change another way to execute command

--- listeners/add.go
+++ listeners/add.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/exec"
@@ -34,6 +35,37 @@ func handleCommandErr(err error) string {
 	return response
 }
 
+func execCommand(commandName string) bool {
+	cmd := exec.Command(commandName)
+
+	fmt.Println(cmd.Args)
+	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	cmd.Start()
+	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+	reader := bufio.NewReader(stdout)
+
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Println(line)
+	}
+
+	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
+	cmd.Wait()
+	return true
+}
+
+
 func Server(listen *net.TCPListener) {
 	for {
 		conn, err := listen.AcceptTCP()
@@ -53,14 +85,20 @@ func Server(listen *net.TCPListener) {
 				fmt.Println("error receiving data from client:", err.Error())
 			} else {
 				var response string
-				var out bytes.Buffer
+				//var out bytes.Buffer
 				command := "ipfs get " + receiveData + " --output=repos/" + receiveData
-				cmd := exec.Command("/bin/bash", "-c", command)
-				cmd.Stdout = &out
-				err2 := cmd.Start()
-				response = handleCommandErr(err2)
-				err2 = cmd.Wait()
-				response = handleCommandErr(err2)
+				getResult := execCommand(command)
+				if getResult == true{
+					response = "success"
+				} else {
+					response = "error"
+				}
+				//cmd := exec.Command("/bin/bash", "-c", command)
+				//cmd.Stdout = &out
+				//err2 := cmd.Start()
+				//response = handleCommandErr(err2)
+				//err2 = cmd.Wait()
+				//response = handleCommandErr(err2)
 				fmt.Println(response)
 
 				conn.Write([]byte(response)) // return the message through the original connection
