fedfb4decc51b703634ad07e44333db07301819f
Nigel <609441862@qq.com>
2018-09-23 11:08:18 +0800

    change another way to get files

--- listeners/add.go
+++ listeners/add.go
@@ -35,36 +35,6 @@ func handleCommandErr(err error) string {
 	return response
 }
 
-func execCommand(commandName string) bool {
-	cmd := exec.Command(commandName)
-
-	fmt.Println(cmd.Args)
-	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
-	stdout, err := cmd.StdoutPipe()
-
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	cmd.Start()
-	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
-	reader := bufio.NewReader(stdout)
-
-	//实时循环读取输出流中的一行内容
-	for {
-		line, err2 := reader.ReadString('\n')
-		if err2 != nil || io.EOF == err2 {
-			break
-		}
-		fmt.Println(line)
-	}
-
-	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
-	cmd.Wait()
-	return true
-}
-
 
 func Server(listen *net.TCPListener) {
 	for {
@@ -87,20 +57,28 @@ func Server(listen *net.TCPListener) {
 				var response string
 				//var out bytes.Buffer
 				command := "ipfs get " + receiveData + " --output=repos/" + receiveData
-				getResult := execCommand(command)
-				if getResult == true{
-					response = "success"
-				} else {
-					response = "error"
+				cmd := exec.Command("/bin/bash", "-c", command)
+				fmt.Println(cmd.Args)
+				stdout, err := cmd.StdoutPipe()
+				if err != nil {
+					fmt.Println(err)
 				}
-				//cmd := exec.Command("/bin/bash", "-c", command)
-				//cmd.Stdout = &out
-				//err2 := cmd.Start()
-				//response = handleCommandErr(err2)
-				//err2 = cmd.Wait()
-				//response = handleCommandErr(err2)
-				fmt.Println(response)
+				cmd.Start()
+				//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+				reader := bufio.NewReader(stdout)
 
+				//实时循环读取输出流中的一行内容
+				for {
+					line, err2 := reader.ReadString('\n')
+					if err2 != nil || io.EOF == err2 {
+						fmt.Println(err2)
+						break
+					}
+					fmt.Println(line)
+				}
+
+				//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
+				cmd.Wait()
 				conn.Write([]byte(response)) // return the message through the original connection
 			}
 
