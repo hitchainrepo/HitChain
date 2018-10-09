cfde37457c6ab84e141b2eba25276bf8dcc5b96a
nigel007 <609441862@qq.com>
2018-10-09 21:45:43 +0800

    remove static_root

--- HDFS_Web/settings.py
+++ HDFS_Web/settings.py
@@ -131,4 +131,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "static_common")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static_common")
